974b0cd721a0a7e2793c71eeed24be43e56998f9
Tijana80 <89365168+Tijana80@users.noreply.github.com>
2021-09-12 19:57:56 +0200

    Update .sql

--- nedelja/.sql
+++ nedelja/.sql
@@ -10,3 +10,10 @@ WHERE region_id=1 AND phone_number IS NOT NULL;
   SELECT dependents.first_name,MAX(employees.salary) AS MaxSalary FROM dependents
   INNER JOIN employees ON dependents.employee_id=employees.employee_id
   GROUP BY dependents.first_name,employees.salary ORDER BY employees.salary DESC LIMIT 1;
+  
+  
+ SELECT  SUM(employees.salary),countries.country_name AS TotalSalary FROM employees
+INNER JOIN departments ON departments.department_id=employees.department_id
+INNER JOIN locations ON locations.location_id=departments.location_id
+INNER JOIN countries ON countries.country_id=locations.country_id
+GROUP BY countries.country_name ORDER BY SUM(employees.salary) DESC; 
